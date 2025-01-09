c8ea334623399f395903b0a6a6b31b2ab7a3adf3
pcScuola <filippo.boschi.008@gmail.com>
2025-01-09 10:57:38 +0100

    inizio esempio WPF

---
+++ EsempioWpf/EsempioWpf/Ristorante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsempioWpf
+{
+    public class Ristorante
+    {
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+        }
+        private int _maxCoperti;
+        public int MaxCoperti
+        {
+            get { return _maxCoperti; }
+            set
+            {
+                if (value < 0) throw new Exception("errore");
+                _maxCoperti = value;
+            }
+        }
+        public Ristorante(string nome, int coperti)
+        {
+            _nome = nome;
+            MaxCoperti = coperti;
+        }
+
+        public override string ToString()
+        {
+            return "RISTORANTE " + _nome.ToUpper() + "numero massimo coperti: " + MaxCoperti;
+        }
+    }
+}
