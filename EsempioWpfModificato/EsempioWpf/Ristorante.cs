1fcfa0cdcc9196e9a52e4f8c8090c45c4d9fa96e
pcScuola <filippo.boschi.008@gmail.com>
2025-01-11 08:05:31 +0100

    Upload di file di esempio di WPF

---
+++ EsempioWpfModificato/EsempioWpf/Ristorante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsempioWpf
+{
+    internal class Ristorante
+    {
+        private string _nome;
+        public string Nome { 
+            get { return _nome; } 
+        }
+
+        private int _maxCoperti;
+        public int MaxCoperti
+        {
+            get { return _maxCoperti; }
+            set {
+                if (value < 0) throw new ArgumentOutOfRangeException();
+                _maxCoperti = value; 
+            }
+        }
+
+        public Ristorante(string nome, int coperti)
+        {
+            _nome = nome;
+            MaxCoperti = coperti;
+        }
+
+        public override string ToString()
+        {
+            return _nome.ToUpper() + " numero massimo coperti: " + MaxCoperti;
+        }
+    }
+}
