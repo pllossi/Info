0c9c6593326482725056071c95da89b91e21bc37
pcScuola <filippo.boschi.008@gmail.com>
2025-01-15 12:45:20 +0100

    verifica

---
+++ VERIFICA_GENNAIO_BOSCHI/VERIFICA_GENNAIO_BOSCHI/Distributore.cs
@@ -0,0 +1,68 @@
+﻿namespace VERIFICA_GENNAIO_BOSCHI
+{
+    public class Distributore
+    {
+        private Prodotto?[] _scompartimenti;
+        public Distributore(Prodotto?[] scompartimenti)
+        {
+            _scompartimenti = scompartimenti;
+        }
+        public Distributore(int numProdotti)
+        {
+            _scompartimenti= new Prodotto?[numProdotti];
+        } 
+        /// <summary>
+        ///Ritorno il prezzo totale
+        /// </summary>
+        /// <returns></returns>
+        public int ValoreTotaleProdotti()
+        {
+            int sum = 0;
+            foreach (Prodotto? prodotto in _scompartimenti)
+            {
+                if (prodotto != null)
+                {
+                    sum += prodotto.PrezzoInCent;
+                }
+            }
+            return sum;
+        }
+        /// <summary>
+        /// Faccio in modo di inserire i prodotti
+        /// </summary>
+        /// <param name="scompartimento"></param>
+        /// <param name="prodotto"></param>
+        /// <exception cref="Exception"></exception>
+        public void InserimentoProdotti(int scompartimento, Prodotto? prodotto)
+        {
+            if (scompartimento <= 0 || scompartimento > _scompartimenti.Length) throw new Exception("Questa scompartimento non esiste!");
+            if (_scompartimenti[scompartimento-1] != null) throw new Exception($"Scompartimento già occupato da : {_scompartimenti[scompartimento-1].NomeProdotto}");
+            _scompartimenti[scompartimento-1]= prodotto;
+        }
+        /// <summary>
+        /// Ritorno un'oggetto in uno scompartimento
+        /// </summary>
+        /// <param name="scompartimento"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public Prodotto? ProdottoNelloScompartimento(int scompartimento)
+        {
+            if (scompartimento <= 0 || scompartimento > _scompartimenti.Length) throw new Exception("Questa scompartimento non esiste!");
+            return _scompartimenti[scompartimento-1];
+        }
+        /// <summary>
+        /// Faccio in modo che in questo metodo ritorno il prezzo di un singolo oggetto
+        /// </summary>
+        /// <param name="scompartimento"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public int PrezzoProdottoSingolo(int scompartimento) {
+            if (scompartimento > 0 && scompartimento < _scompartimenti.Length) {
+                if (_scompartimenti[scompartimento - 1] == null) throw new Exception("Prodotto non esistente");
+                return _scompartimenti[scompartimento - 1].PrezzoInCent;
+            }
+            throw new Exception("Questa scompartimento non esiste!");
+        }
+
+    }
+}
---
+++ VERIFICA_GENNAIO_BOSCHI/VERIFICA_GENNAIO_BOSCHI/Prodotto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VERIFICA_GENNAIO_BOSCHI
+{
+    public enum TipoProdotto
+    {
+        Merendina,
+        Snacks,
+        Bevanda
+    }
+    public class Prodotto : IComparable<Prodotto>
+    {
+        public TipoProdotto TipoDiProdotto
+        {
+            get;
+        }
+        private string _nomeProdotto;
+        public string NomeProdotto
+        {
+            get
+            {
+                return _nomeProdotto;
+            }
+            set
+            {
+                if (value == null || value == "" || value == " ") throw new ArgumentException("Il nome non può essere ne vuoto ne bianco ne null");
+                _nomeProdotto = value;
+            }
+        }
+        //lo faccio in centesimi il prezzo per evitare errori legati all'arrotondamento
+        private int _prezzoInCent;
+        public int PrezzoInCent
+        {
+            get
+            {
+                return _prezzoInCent;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("il prezzo deve essere maggiore di zero");
+                _prezzoInCent = value;
+            }
+        }
+        public Prodotto(string nomeProdotto,int prezzoInCent,TipoProdotto tipoDiProdotto)
+        {
+            PrezzoInCent=prezzoInCent;
+            NomeProdotto = nomeProdotto;
+            TipoDiProdotto = tipoDiProdotto;
+        }
+
+        public int CompareTo(Prodotto? other)
+        {
+            if(other == null) return 1;
+            return PrezzoInCent.CompareTo(other.PrezzoInCent);
+        }
+    }
+}
