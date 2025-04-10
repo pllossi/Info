b0d538c910d5c5f5f2a3e3ee81a147e95b0f1591
pcScuola <filippo.boschi.008@gmail.com>
2025-04-10 10:58:44 +0200

    DA FINIRE CEASER

---
+++ Ceasar_Cipher/Ceasar_Cipher/Cipher.cs
@@ -0,0 +1,27 @@
+﻿namespace Ceasar_Cipher
+{
+    public class Cipher
+    {
+        public string Enrc(string toEnrc,int shift)
+        {
+            string Encript = "";
+            foreach(char c in toEnrc)
+            {
+                char charact = (char)(c + shift);
+                Encript += charact;
+                
+            }
+            return Encript;
+        }
+        public string Decrypt(string toDecrypt, int shift)
+        {
+            string Decript = "";
+            foreach (char c in toDecrypt)
+            {
+                char charact = (char)(c - shift);
+                Decript += charact;
+            }
+            return Decript;
+        }
+    }
+}
