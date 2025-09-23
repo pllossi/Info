a4e6e2b353e88c6ae79244f4c2c2e53f3d8e5a62
pllossi <115420435+pllossi@users.noreply.github.com>
2025-09-23 09:44:57 +0200

    enum

--- Noleggio/NoleggioLib/Bicicletta.cs
+++ Noleggio/NoleggioLib/Bicicletta.cs
@@ -8,7 +8,19 @@ namespace NoleggioLib
 {
     public class Bicicletta : Veicolo
     {
-        public Bicicletta(string Targa, int CostoAlGiorno) : base(Targa, CostoAlGiorno) { }
+        public TipoCasco Casco { get; private set; }
+        public Bicicletta(string Targa, int CostoAlGiorno, string casco) : base(Targa, CostoAlGiorno) 
+        {
+            if (casco == null|| (casco.ToLower() != "integrale" && casco.ToLower() != "jet" && casco.ToLower() !="semi integrale"))
+                throw new ArgumentNullException("Casco errato");
+            casco = casco.ToLower();
+            switch (casco)
+            {
+                case ("jet"): Casco = TipoCasco.Jet; break;
+                case ("integrale"): Casco=TipoCasco.Integrale; break;
+                default: Casco= TipoCasco.Semi_Integrale; break;
+            }
+        }
         public void InizioNoleggio(DateOnly inizio)
         {
             dataInizioNoleggio = inizio;
@@ -22,7 +34,7 @@ namespace NoleggioLib
         }
         public override string ToString()
         {
-            return $"Bicicletta: {targa}, Costo al giorno: {costoAlGiorno}€";
+            return $"Bicicletta: {targa}, Costo al giorno: {costoAlGiorno}€, Tipo Casco {Casco}";
         }
     }
 }
