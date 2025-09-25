baf91798fb639a68c0e69040cc43f7e62f88e5b2
pllossi <115420435+pllossi@users.noreply.github.com>
2025-09-25 10:55:40 +0200

    Inizio esercizio di GestioneAnimaliDomestici (da rinominare)

---
+++ GestoreAnimaliDomestici/Domain/Model/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model
+{
+    public abstract class Animal
+    {
+        private string _name;
+        public string Name {
+            get => _name; 
+            private set
+            {
+                if (String.IsNullOrEmpty(_name))
+                    throw new ArgumentNullException(nameof(_name));
+                _name = value;
+            }
+        }
+        public VeterninaryVisit[] veterninaryVisits { get; private set; }
+        private string _favouriteGame;
+        public string FavouriteGame
+        {
+            get => _favouriteGame;
+            set
+            {
+                if (String.IsNullOrEmpty(_favouriteGame))
+                    throw new ArgumentException(nameof(_favouriteGame));
+                _favouriteGame = value;
+            }
+        }
+
+    }
+}
