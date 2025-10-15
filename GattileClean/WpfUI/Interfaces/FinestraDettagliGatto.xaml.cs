182b382558b833945b369435cecd4e4fe9b8cbe6
pllossi <115420435+pllossi@users.noreply.github.com>
2025-10-15 09:24:36 +0200

    Creazione di GattileClean con i files da Gattile

---
+++ GattileClean/WpfUI/Interfaces/FinestraDettagliGatto.xaml.cs
@@ -0,0 +1,43 @@
+﻿using GattileLib;
+using System;
+using System.Windows;
+
+namespace Gattile
+{
+    /// <summary>
+    /// Logica di interazione per FinestraDettagliGatto.xaml
+    /// </summary>
+    public partial class FinestraDettagliGatto : Window
+    {
+        public FinestraDettagliGatto(Gatto gatto)
+        {
+            InitializeComponent();
+            DataContext = new GattoDettagliViewModel(gatto);
+        }
+
+        private void BtnChiudi_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+
+    public class GattoDettagliViewModel
+    {
+        public string Nome { get; }
+        public string Razza { get; }
+        public string Sesso { get; }
+        public DateTime? DataNascita { get; }
+        public string? Descrizione { get; }
+        public string CodiceIdentificativo { get; }
+
+        public GattoDettagliViewModel(Gatto gatto)
+        {
+            Nome = gatto.Nome;
+            Razza = gatto.Razza;
+            Sesso = gatto.Maschio ? "Maschio" : "Femmina";
+            DataNascita = gatto.DataNascita;
+            Descrizione = gatto.Descrizione;
+            CodiceIdentificativo = gatto.CodiceId;
+        }
+    }
+}
---
+++ GattileClean/WpfUI/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Gattile;
+using System.Windows;
+using System.ComponentModel;
+
+namespace Gattile
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        GestoreGattile gestore = new GestoreGattile();
+
+        private int _numeroGatti;
+        public int NumeroGatti
+        {
+            get => _numeroGatti;
+            set
+            {
+                if (_numeroGatti != value)
+                {
+                    _numeroGatti = value;
+                    OnPropertyChanged(nameof(NumeroGatti));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            AggiornaNumeroGatti();
+        }
+
+        public void AggiornaNumeroGatti()
+        {
+            NumeroGatti = gestore.gattiPresenti.Count;
+        }
+
+        private void btnVisualizzaGatti_Click(object sender, RoutedEventArgs e)
+        {
+            var finestra = new FinestraGatti(gestore);
+            finestra.Show();
+        }
+
+        private void btnNuovoGatto_Click(object sender, RoutedEventArgs e)
+        {
+            var finestra = new FinestraNuovoGatto(gestore);
+            finestra.ShowDialog();
+            AggiornaNumeroGatti();
+        }
+
+        private void btnVisualizzaAdozioni_Click(object sender, RoutedEventArgs e)
+        {
+            var finestra = new FinestraAdozioni(gestore);
+            finestra.ShowDialog();
+        }
+
+        private void btnNuovaAdozione_Click(object sender, RoutedEventArgs e)
+        {
+            var finestra = new FinestraNuovaAdozione(gestore);
+            finestra.ShowDialog();
+            AggiornaNumeroGatti();
+        }
+
+        private void btnAdozioneFallita_Click(object sender, RoutedEventArgs e)
+        {
+            var finestra = new FinestraAdozioneFallita(gestore);
+            finestra.ShowDialog();
+            AggiornaNumeroGatti();
+        }
+
+        private void btnNuovoAdottante_Click(object sender, RoutedEventArgs e)
+        {
+            var finestra = new FinestraNuovoAdottante(gestore);
+            finestra.ShowDialog();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
