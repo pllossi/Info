2ff992581a3e86195703389306308e5c32cc42a9
pllossi <115420435+pllossi@users.noreply.github.com>
2025-10-20 15:56:39 +0200

    Spostamento del domain dalla cartella principale a src

---
+++ Gattile/src/Domain/Entities/Adozione.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Entities
+{
+    public class Adozione
+    {
+        public Adozione(Adottante adottante, Gatto gatto, DateTime dataAdozione)
+        {
+            Adottante = adottante;
+            Gatto = gatto;
+            DataAdozione = dataAdozione;
+        }
+
+        public Gatto Gatto
+        {
+            get => _gatto;
+            private set
+            {
+                _gatto = value ?? throw new ArgumentNullException(nameof(value));
+            }
+        }
+        private Gatto _gatto;
+        public Adottante Adottante
+        {
+            get => _adottante;
+            private set
+            {
+                _adottante = value ?? throw new ArgumentNullException(nameof(value));
+            }
+        }
+        private Adottante _adottante;
+        public DateTime DataAdozione { get; private set; }
+    }
+}
