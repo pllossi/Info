5a0c6d0de26c161655fa31f0e2f4bc46037ff658
pllossi <pllossi@PC4610>
2025-10-23 10:59:58 +0200

    Dto's

---
+++ Gattile/src/Application/DTO/AdopterDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO
+{
+    public record AdopterDTO(
+        string Name,
+        string PhoneNumber,
+        string Email,
+        string Address
+        );
+}
